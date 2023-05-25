3fba75ec434967087ca96663324bdaa379cd7c35
mattke0301 <59602539+mattke0301@users.noreply.github.com>
2023-05-25 12:05:06 +0800

    Add files via upload

---
+++ test.m
@@ -0,0 +1,5 @@
+
+%in_dir='D:\AICUP\Private Testing Dataset\test_data_private'; % Specify directory of wavs
+in_dir='D:\covarep-1.4.2\123'; % Specify directory of wavs
+sample_rate=0.01; % State feature sampling rate
+COVAREP_feature_extraction(in_dir,sample_rate); % Launch feature extraction
